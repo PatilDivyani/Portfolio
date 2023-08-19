61cd6c88df670b00a01c1fe96448b9fe864c9e85
patildivyani <patildivyani30@gmail.com>
2023-08-19 19:31:44 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <img src={UserImg} alt="img" />
       </div>
       <div className="about-details">
-        <h1 className="about-title">About Me</h1>
+        <h1 className="about-title">नमस्कार &#128591;</h1>
         <h3 className="about-subtitle">I am Divyani Patil...!</h3>
         <p className="about-description">
           I'm a dedicated Frontend Developer with a strong focus on ReactJS. Skilled in HTML, CSS, and JavaScript, I thrive on creating captivating user experiences through dynamic and responsive web applications. I love learning and staying up-to-date with the latest trends in ReactJS to deliver innovative and impactful solutions. Let's connect and explore exciting opportunities in the world of frontend development!
